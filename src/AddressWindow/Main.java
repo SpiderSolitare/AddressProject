c26227937e20ffa46ede92ccf7a91bd6f6a82675
BuildTools <unconfigured@null.spigotmc.org>
2020-05-13 17:16:32 +0200

    alles

--- src/AddressWindow/Main.java
+++ src/AddressWindow/Main.java
@@ -49,9 +49,7 @@ public class Main extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 368, 310);
